4788757414de9641e832858278f00b0daba2b12e
daovantai <daotai2309@gmail.com>
2022-10-09 21:11:48 +0700

    tb

--- webelectro/src/main/java/web/WebelectroApplication.java
+++ webelectro/src/main/java/web/WebelectroApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebelectroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebelectroApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(WebelectroApplication.class, args);
 	}
 
 }
